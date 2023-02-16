9e8099b767faa94e875394d749388c4052b02619
Tarık Yıldız <112180001+tarik-yildiz@users.noreply.github.com>
2023-02-16 14:59:36 +0300

    oracle connection added

--- HazelcastDemo.java
+++ HazelcastDemo.java
@@ -4,6 +4,13 @@ package com.mycompany.hazelcastdemo;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 /**
  *
@@ -11,8 +18,8 @@ import java.util.Random;
  */
 public class HazelcastDemo {
 
-    public static void main(String[] args) {
-        HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient();
+    public static void main(String[] args) throws SQLException {
+    HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient();
     IMap<Integer, Integer> map = hazelcastInstance.getMap("myMap");
     Random rand = new Random();
     int numberOfEntries = 20000;
@@ -28,7 +35,44 @@ public class HazelcastDemo {
       int value = map.get(key);
     }
     endTime = System.currentTimeMillis();
-    System.out.println("Time to get " + numberOfEntries + " entries: " + (endTime - startTime) + "ms");
-	hazelcast.shutdown();
+    System.out.println("Time to get " + numberOfEntries + " entries: " + (endTime - startTime) + "ms");    
+    String url="jdbc:oracle:thin:@//localhost:1521/XE";
+    String user="system";
+    String password="oracle";
+    try{
+        Class.forName("oracle.jdbc.OracleDriver");
+        System.out.println("Load success");
+        Connection con= DriverManager.getConnection(url,user,password);
+        System.out.println("Baglanti basarili"+con);
+        rand = new Random();
+        numberOfEntries = 50000;
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < numberOfEntries; i++) {
+            String query="insert into DB2 (SAYİ) values ('"+rand.nextInt(2000)+"') ";
+            Statement stmt=con.createStatement();
+            stmt.execute(query);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time to put " + numberOfEntries + " entries: " + (endTime - startTime) + "ms");
+
+            List<String> list=new ArrayList<>();
+            startTime = System.currentTimeMillis();
+            Statement stmt=con.createStatement();
+            ResultSet rs;
+            rs=stmt.executeQuery("Select * from DB2");
+            int sayac=0;
+            while (rs.next()) {
+                String sayi = rs.getString("SAYİ");
+                list.add(sayi);
+                sayac++;
+            }
+            endTime = System.currentTimeMillis();
+
+            System.out.println("Time to get " + sayac + " entries: " + (endTime - startTime) + "ms");
+
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+
+        }
     }
 }
